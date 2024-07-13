f7f3179052fad4f86d43ed0ea9a9fd10e4260f20
Aniparvin <aniparvin357@gmail.com>
2024-07-13 13:18:31 +0530

    read csv file

--- purchases.py
+++ purchases.py
@@ -2,5 +2,8 @@ import pandas as pd
 data={'apples':[3,2,0,1],'oranges':[0,3,7,2]}
 purchases=pd.DataFrame(data)
 print(purchases)
-purchases=pd.DataFrame(data,index=['Jane','Robert','Lilly','David'])
+purchases=pd.DataFrame(data,index=['June','Robert','Lilly','David'])
 print(purchases)
+purchases.loc['June']
+df=pd.read_csv(r"C:\Users\az\Downloads\purchases(1).csv")
+print(df)
