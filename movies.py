368091bb0d3ba4db3f518a57395ec50b375fc2a0
Aniparvin <aniparvin357@gmail.com>
2024-07-22 11:55:50 +0530

    ..

--- movies.py
+++ movies.py
@@ -1,3 +1,6 @@
 import pandas as pd
 movies_df=pd.read_csv(r"C:\\Users\\az\Downloads\\IMDB-Movie-Data.csv",index_col="Title")
 print(movies_df.head())
+movies_df.tail(2)
+movies_df.info()
+movies_df.shape
